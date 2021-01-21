e842b8263e17805f88556e812bd7c00ab633d388
Nadim Hallaq <nadimhallaq@gmail.com>
2021-01-21 14:42:36 -0500

    second commit with manifest fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `nourinsurance`,
+        short_name: `nourinsurance`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/components/icons/NourLogo.js`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
 
